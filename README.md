79bc47d774da28f5e9797415dd4724374086a093
mado11x11 <depass.25u17@gmail.com>
2023-04-17 03:00:34 +0900

    RestTemplateをコンストラクタインジェクションを利用した呼び出しに変更

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ### 概要  
 
 ----
-Spring Bootを使って画面にHello Worldを表示    
+Spring bootを使って画面にHello Worldを表示    
 Hello Worldを表示したプロジェクトに自分で考えた変更を追加  
 ⇒天気の外部APIから今日の東京の天気を取得して表示する
 
--- src/main/java/com/example/helloworld2023/HelloWorldController.java
+++ src/main/java/com/example/helloworld2023/HelloWorldController.java
@@ -1,13 +1,16 @@
 package com.example.helloworld2023;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class HelloWorldController {
-    private final HelloWorldForm helloWorldForm = new HelloWorldForm();
-    private final  HelloWorldService helloWorldService =new HelloWorldService();
+
+    @Autowired
+    private  HelloWorldService  helloWorldService;
 
     @GetMapping("/")
     public String hello(Model model) {
