51157dc2b680a6e8643b5339cf86cf99a72897ec
mado11x11 <depass.25u17@gmail.com>
2023-04-17 15:27:19 +0900

    HelloWorldServiceをコンストラクタインジェクションを利用した呼び出しに変更

--- src/main/java/com/example/helloworld2023/HelloWorldController.java
+++ src/main/java/com/example/helloworld2023/HelloWorldController.java
@@ -1,16 +1,17 @@
 package com.example.helloworld2023;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class HelloWorldController {
 
-    @Autowired
-    private  HelloWorldService  helloWorldService;
+    private  final  HelloWorldService helloWorldService;
+
+    public HelloWorldController(HelloWorldService helloWorldService){
+        this.helloWorldService=helloWorldService;
+    }
 
     @GetMapping("/")
     public String hello(Model model) {
